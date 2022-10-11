719c3b73a6a331171186a2ae97024be9cbf7d0d1
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-11 23:19:23 +0900

    Finished until #3.4
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("<h1>I love you</h1>");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("You are in login");
+};
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+app.listen(PORT, handleListening);
