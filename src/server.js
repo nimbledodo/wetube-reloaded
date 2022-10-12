98b034dc9d68858fb77771598431fb63eba16ace
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-12 23:09:01 +0900

    Finished upto Ch 3.
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/server.js
+++ src/server.js
@@ -3,17 +3,36 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+
+  console.log("Allowed, you may continue");
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("<h1>I love you</h1>");
+  return res.send("I love middlewares");
 };
+// const handleLogin = (req, res) => {
+//   return res.send("You are in login");
+// };
 
-const handleLogin = (req, res) => {
-  return res.send("You are in login");
-};
-
+// const handleProtected = (req, res) => {
+//   return res.send("Welcmome to the private lounge.");
+// };
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+// app.get("/protected", handleProtected);
 app.listen(PORT, handleListening);
