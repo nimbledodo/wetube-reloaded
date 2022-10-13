40cf83d48991ed0b5ab87d32ca1fef935167f849
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-13 22:57:32 +0900

    finished upto #5.1
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import express from "express";
 
-export const trending = (req, res) => res.send("Hpme page Videos");
-export const watch = (req, res) => res.send(`Watch Video #${req.params.id}`);
+export const trending = (req, res) => res.render("home");
+export const watch = (req, res) => res.render("watch");
 export const edit = (req, res) => res.send("Edit Video");
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
