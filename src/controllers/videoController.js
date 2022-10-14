61e993d2829744ea3c6ced3d0c50b7f28d050e3e
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-14 23:42:10 +0900

    upto #6.4
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,42 +1,57 @@
 import express from "express";
 
-const fakeUser = {
-  username: "Nicolas",
-  loggedIn: false,
-};
+let videos = [
+  {
+    title: "Hello",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Video #2",
+    rating: 4,
+    comments: 20,
+    createdAt: "2 hours ago",
+    views: 60,
+    id: 2,
+  },
+  {
+    title: "Whatsup",
+    rating: 5,
+    comments: 3,
+    createdAt: "1 week ago",
+    views: 100,
+    id: 3,
+  },
+];
 
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Hello",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #2",
-      rating: 4,
-      comments: 20,
-      createdAt: "2 hours ago",
-      views: 60,
-      id: 2,
-    },
-    {
-      title: "Whatsup",
-      rating: 5,
-      comments: 3,
-      createdAt: "1 week ago",
-      views: 100,
-      id: 3,
-    },
-  ];
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser, videos });
+  res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watch = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-export const search = (req, res) => res.send("Search Video");
-export const upload = (req, res) => res.send("Upload Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", {
+    pageTitle: `Watching ${video.title}`,
+    video,
+  });
+};
+
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("edit", {
+    pageTitle: `Editing ${video.title}`,
+    video,
+  });
+};
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
--- src/routers/globalRouters.js
+++ src/routers/globalRouters.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { join, login } from "../controllers/userController.js";
-import { trending, search } from "../controllers/videoController.js";
+import { trending } from "../controllers/videoController.js";
 
 const globalRouter = express.Router();
 
@@ -9,6 +9,5 @@ const handleHome = (req, res) => res.send("Home");
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 
 export default globalRouter;
