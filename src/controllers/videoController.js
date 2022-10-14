f848cc90fe2c59ab43fcaca6c455afdaf9f2521e
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-14 22:51:38 +0900

    upto #5.9
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,8 +6,32 @@ const fakeUser = {
 };
 
 export const trending = (req, res) => {
-  //   const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const videos = [];
+  const videos = [
+    {
+      title: "Hello",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video #2",
+      rating: 4,
+      comments: 20,
+      createdAt: "2 hours ago",
+      views: 60,
+      id: 2,
+    },
+    {
+      title: "Whatsup",
+      rating: 5,
+      comments: 3,
+      createdAt: "1 week ago",
+      views: 100,
+      id: 3,
+    },
+  ];
   res.render("home", { pageTitle: "Home", fakeUser: fakeUser, videos });
 };
 
