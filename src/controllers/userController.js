8ceacfcd3d3358e61c194f0e807ff58a85fda56d
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-18 22:54:57 +0900

    #7.21 Github Login end
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -102,11 +102,30 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    const email = emailData.find(
+    const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
     );
-    if (!email) {
+    if (!emailObj) {
       return res.redirect("/login");
+    }
+    const existingUser = await User.findOne({ email: emailObj.email });
+    console.log(`existingUser: ${existingUser}`);
+    if (existingUser) {
+      req.session.loggedIn = true;
+      req.session.user = existingUser;
+      res.redirect("/");
+    } else {
+      const user = await User.create({
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+      req.session.loggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
     }
   } else {
     return res.redirect("/login");
