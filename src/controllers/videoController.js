eac5502fc9cb6e817602381765fbb50972919dfb
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-23 00:24:52 +0900

    #8.14 Bug fixes
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,18 +31,25 @@ export const watch = async (req, res) => {
 
 export const getEdit = async (req, res) => {
   const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
   const video = await Video.findById(id);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
+  if (String(video.owner) !== String(_id)) {
+    //type이 다르기 때문에 String으로 만들어서 비교함
+    return res.status(403).redirect("/");
+  }
   return res.render("edit", { pageTitle: video.title, video });
-  res.render("edit", {
-    pageTitle: `Editing `,
-  });
 };
 
 export const postEdit = async (req, res) => {
   const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   // lowercase video: video object in my database
@@ -50,7 +57,10 @@ export const postEdit = async (req, res) => {
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
-
+  if (String(video.owner) !== String(_id)) {
+    //type이 다르기 때문에 String으로 만들어서 비교함
+    return res.status(403).redirect("/");
+  }
   await Video.findByIdAndUpdate(id, {
     title,
     description,
@@ -92,6 +102,17 @@ export const postUpload = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
   const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.status(404).render("404", { pageTitle: "Video not found." });
+  }
+  if (String(video.owner) !== String(_id)) {
+    //type이 다르기 때문에 String으로 만들어서 비교함
+    return res.status(403).redirect("/");
+  }
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
 };
--- src/models/User.js
+++ src/models/User.js
@@ -14,7 +14,9 @@ const userSchema = new mongoose.Schema({
 
 //encrypt password before saving
 userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 5);
+  if (this.isModified("password"))
+    //video를 upload할 때마다 user.save가 발생해서 password가 여러번 hash 되는 것을 방지하기 위함
+    this.password = await bcrypt.hash(this.password, 5);
 });
 
 const User = mongoose.model("User", userSchema);
