1579b0b0bb6188eaf77efd46066476762a8b873b
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-23 17:26:59 +0900

    #9.5 MiniCssExtractPlugin
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/client/js/main.js",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   mode: "development",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
   },
   module: {
     rules: [
@@ -20,7 +26,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         // 마지막에 사용하는 것부터 먼저 적어야 함
         // sass-loader: scss-> css변경
         //css-loader: css파일을 읽어서 javascript에서 사용가능한 string으로 반환
