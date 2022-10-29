c1b196e7e217dd9c1f11aad448d8548c4d880e3b
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-29 22:15:52 +0900

    #11.0 Video Player Start
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/client/js/main.js",
+  entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
@@ -11,7 +14,7 @@ module.exports = {
   mode: "development",
   watch: true, //이렇게 하면 매번 npm run assets를 다시 시작하지 않아도 됨
   output: {
-    filename: "js/main.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true, // 이렇게 하면 지난 것을 지우고 다시 만듬
   },
