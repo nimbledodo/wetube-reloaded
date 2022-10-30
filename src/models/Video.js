2df084ad1ad1d7f3534c6fba9b214873c4cd6cdd
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-10-30 15:43:12 +0900

    #14 Webassembly video transcode
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/models/Video.js
+++ src/models/Video.js
@@ -4,7 +4,7 @@ const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 80 },
   fileUrl: { type: String, required: true },
   thumbUrl: { type: String, required: true },
-  description: { type: String, trim: true, minLength: 20 },
+  description: { type: String, trim: true, minLength: 5 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
