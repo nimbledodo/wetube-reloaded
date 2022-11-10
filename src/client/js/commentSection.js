f8206041f7319b43444e2c780ee04e74fa215c2e
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-11-10 23:41:20 +0900

    #16.7 Realtime comments
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,12 +1,24 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
 const handleCommentSubmit = async (e) => {
   e.preventDefault();
   const textarea = form.querySelector("textarea");
   const videoId = videoContainer.dataset.id;
   const text = textarea.value;
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -14,7 +26,11 @@ const handleCommentSubmit = async (e) => {
     body: JSON.stringify({ text: text }),
   });
   textarea.value = "";
-  window.location.reload();
+  if (status === 201) {
+    addComment(text);
+  }
+
+  // window.location.reload();
 };
 
 form.addEventListener("submit", handleCommentSubmit);
