41c707282c0805ff7838af2a04410c4f774a57f0
Jeehyang Huh <jeehyang.huh@gmail.com>
2022-11-29 23:37:25 +0900

    comment app.use (cross-origin..)
    Signed-off-by: Jeehyang Huh <jeehyang.huh@gmail.com>

--- src/server.js
+++ src/server.js
@@ -18,11 +18,11 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 
-app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Cross-Origin-Embedder-Policy", "require-corp");
+//   res.header("Cross-Origin-Opener-Policy", "same-origin");
+//   next();
+// });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // to make backend understands json.
